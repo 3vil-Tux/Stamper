b9806eab52cd000f6dc9a69bab130eea6f153677
3vil-Tux <74163230+3vil-Tux@users.noreply.github.com>
2020-11-25 20:24:56 -0500

    Create colors.py

---
+++ stamper/colors.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+'''Disclaimer'''
+# Copyright 2020 3vil.Tux @ https://github.com/3vil-Tux
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+'''Import(s)'''
+
+'''Codes(s)'''
+good     = '\033[38;2;71;194;60m'
+info     = '\033[38;2;53;78;204m'
+warning  = '\033[38;2;232;144;21m'
+bad      = '\033[38;2;245;66;66m'
+reset    = '\033[0m'
